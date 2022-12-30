3c248fe4cab98ceefa06e8385afeecbaf3136bf9
yu5 <fukanoyugo+dev@gmail.com>
2022-12-30 13:52:11 +0900

    鸚鵡返しするようにした

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
+require 'bundler/setup'
 require 'sinatra'
+Dotenv.load 
+
+
 class App < Sinatra::Base
-    get '/' do
-        "Hello Wrold!"
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  post '/callback' do
+    body = request.body.read
+  
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
     end
+  
+    events = client.parse_events_from(body)
+  
+    events.each do |event|
+        case event
+        when Line::Bot::Event::Message
+            case event.type
+            when Line::Bot::Event::MessageType::Text
+            message = {
+                type: 'text',
+                text: event.message['text']
+            }
+            client.reply_message(event['replyToken'], message)
+            end
+        end
+    end
+  
+    "OK"
 end
